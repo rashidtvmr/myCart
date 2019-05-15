4d7d8947a2cb7cc3966c9369c9f2ea581b277e4e
Rashidtvmr <rashis.pbt@gmail.com>
2019-05-16 00:07:23 +0530

    mian.js code added and some function in ligin.ejs and signup.ejs are moduled to main.js

---
+++ public/js/main.js
@@ -0,0 +1,39 @@
+//Typed......
+typeIt = (text, element, speed) => {
+  let c = 0;
+  let txt = text;
+
+  let con = setInterval(() => {
+    let txtlen = txt.length;
+    c++;
+    element.innerHTML = txt.substr(0, c) + "|";
+    // element.innerHTML = txt.substr(0, c) + '/';
+    if (c == txtlen) {
+      element.innerHTML = txt.substr(0, c);
+      stop();
+    }
+  }, speed);
+  stop = () => {
+    clearInterval(con);
+  };
+};
+//Typed......
+
+//Validation methods
+showAlert = (Box, Alert, formName) => {
+  Alert.style.display = "block";
+  Box.style.border = "1px solid red";
+  document[formName].submit.disabled = true;
+};
+hideAlert = (validator, Box, Alert, e) => {
+  if (validator) {
+    Box.style.border = "none";
+    e.target.style.background = "none";
+    Alert.style.display = "none";
+  }
+};
+validate = (isValid, formName) => {
+  if (isValid) {
+    document[formName].submit.disabled = false;
+  }
+};
